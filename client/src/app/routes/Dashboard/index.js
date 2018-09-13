50290522aaa829adf7f829b9743c415b503cdedb
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-13 13:14:33 +0530

    Add section title for the card area

--- client/src/app/routes/Dashboard/index.js
+++ client/src/app/routes/Dashboard/index.js
@@ -8,7 +8,7 @@ class Dashboard extends Component {
   render(){
     return(
       <div className="dashboard">
-        <CardArea>
+        <CardArea title="POPULAR MOVIES">
           <MovieCard />
           <MovieCard />
         </CardArea>
