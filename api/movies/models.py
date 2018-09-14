eb73623b6965e558b94551e7ef2a6aa5b9260e6d
Nijinsha <mail.nijinsha@gmail.com>
2018-09-14 16:57:39 +0530

    Add movie, cast, actor and genre models

--- api/movies/models.py
+++ api/movies/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    """
+    Class that represents the genre of the movie.
+    """
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100,default=None,blank=True)
+
+
+class Actor(models.Model):
+    """
+    Class that represents the actor details.
+    """
+    name = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    """
+    Class that represents the Movie details.
+    """
+    title = models.CharField(max_length=100)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    poster = models.URLField()
+    actor = models.CharField(max_length=100)
+
+
+class Cast(models.Model):
+    """
+    Class that represents the cast that appeared in a movie.
+    """
+    actor = models.ForeignKey(Actor)
+    movie = models.ForeignKey(Movie)
