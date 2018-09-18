53586db2ed1042115b998f4de7d3adf93d44d09c
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-18 11:54:39 +0530

    Validate props and handle the loading props

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Dashboard from './routes/Dashboard';
 import UserProfile from './routes/UserProfile';
 import NavBar from '../components/NavBar';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchLoading:false
+    }
+  }
+
   render(){
     const { match } = this.props;
+    const { searchLoading } = this.state;
     return(
       <div>
-        <NavBar />
+        <NavBar 
+          loadingTrue={()=>{this.setState({searchLoading:true})}}
+          loadingFalse={()=>{this.setState({searchLoading:false})}}
+        />
         <div>
           <Switch>
-            <Route exact path={`${match.url}`} component={ Dashboard } />
-            <Route exact path={`${match.url}profile`} component={ UserProfile } />
+            <Route
+              exact
+              path={`${match.url}`}
+              render={(props)=><Dashboard {...props} searchLoading={searchLoading} />}
+            />
+            <Route exact path={`${match.url}profile`} component={UserProfile} />
           </Switch>
         </div>
       </div>
@@ -21,3 +37,14 @@ class App extends Component {
   }
 }
 export default App;
+
+App.defaultProps = {
+  match: { params: { url:''}}
+};
+App.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      url: PropTypes.node,
+    }).isRequired,
+  })
+};
