e0e5ef76e5b631111c4594fe472a7f8439314bbe
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-18 13:21:18 +0530

    Change CardArea to functional component

--- client/src/components/CardArea/index.js
+++ client/src/components/CardArea/index.js
@@ -1,23 +1,46 @@
-import React, { Component} from 'react';
+import React from 'react';
 import { Divider, Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import '../../styles/components/_cardarea.css';
 
-class CardArea extends Component {
-  render(){
+const CardArea = (props) => {
+  const { title, isLoading, children } = props;
     return(
       <div className="main">
         <Divider />
         <div className="title">
-          <h4>{this.props.title}</h4>
+          <h4>{title}</h4>
         </div>
-        <div className="card-space">
-            <Grid container flex-direction="row" spacing={8}>
-                {this.props.children}
-            </Grid>
-        </div>
+        {
+          isLoading? 
+            (
+              <div className="loading-space">
+                {children}
+              </div>
+            )
+          :
+            (
+              <div className="card-space">
+                <Grid container flex-direction="row" spacing={8}>
+                  {children}
+                </Grid>
+              </div>
+            )
+        }
         <Divider />
       </div>
-    );
-  }
+    );  
 }
 export default CardArea;
+
+CardArea.defaultProps = {
+  isLoading: false,
+  title: '',
+  children: <div />
+};
+
+CardArea.propTypes = {
+  isLoading: PropTypes.bool,
+  title: PropTypes.string,
+  children: PropTypes.node,
+};
