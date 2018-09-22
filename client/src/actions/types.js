5f1ff70aaeb05c89a1cd1d0436c323af1ae548f6
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-22 17:33:04 +0530

    Add action types of movie fetch

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,14 @@
+//Navbar actions
 export const NAV_SEARCH = 'NAV_SEARCH';
 export const NAV_SEARCH_SCREEN = 'NAV_SEARCH_SCREEN';
 export const NAV_SEARCH_LOADING = 'NAV_SEARCH_LOADING';
+
+// Fetch Movies
 export const FETCH_POPULAR = 'FETCH_POPULAR';
+export const FETCH_FAVORITE = 'FETCH_FAVORITE';
+export const FETCH_WATCHED = 'FETCH_WATCHED';
+export const FETCH_WATCHLATER = 'FETCH_WATCHLATER';
+export const FETCH_TRENDING = 'FETCH_TRENDING';
+
+
+
