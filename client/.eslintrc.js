ce3db1f479e91605ce79085d0111155e9c8fe381
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-22 18:21:48 +0530

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
       "no-mixed-spaces-and-tabs": [2, "smart-tabs"],
       "import/no-unresolved": "off"
-    }
+    },
+    "parser": "babel-eslint"
 
 };
