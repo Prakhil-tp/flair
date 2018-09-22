c5c3f47282634af07a1cf074dce1e74c405f2ddf
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-22 17:30:22 +0530

    Add TrendingMovie component

---
+++ client/src/app/routes/Dashboard/components/TrendingMovies.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import PropTypes, { shape } from 'prop-types'
+import shortid from 'shortid'
+import MovieCard from 'components/MovieCard'
+import CardArea from 'components/CardArea'
+
+const TrendingMovies = (props) => {
+  const { Movies } = props;
+  if(Movies.length > 0){
+    return (
+      <CardArea title="TRENDING MOVIES">
+        {
+          Movies.map(Movie=>(
+            <MovieCard
+              key={shortid.generate()}
+              title={Movie.title}
+              poster={Movie.poster}
+              genre={Movie.genre}
+              rating={Movie.rating}
+            />
+          ))
+        }
+      </CardArea>
+    )
+  }
+  return <div />
+}
+
+
+const mapStateToProps = state => ({
+  Movies: state.Movies.trendingMovies
+})
+
+TrendingMovies.propTypes = {
+  Movies: PropTypes.arrayOf(
+    shape({
+      title:PropTypes.string,
+      poster:PropTypes.node,
+      genre:PropTypes.string,
+      rating:PropTypes.node
+    })
+    ).isRequired
+}
+
+export default connect(mapStateToProps,{})(TrendingMovies)
