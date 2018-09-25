5e02d509f5de97509e1594c68daf2c586bf160a4
Nijinsha <mail.nijinsha@gmail.com>
2018-09-25 16:19:44 +0530

    Adds jwt authentication

--- api/flair/urls.py
+++ api/flair/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('movies/',include('movies.urls'))
+    path('movies/',include('movies.urls')),
+    path('token/', obtain_jwt_token),
 ]
