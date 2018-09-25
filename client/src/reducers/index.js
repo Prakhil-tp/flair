28a8cda60fba86d4a0341ba59f5f18948348f365
Prakhil-tp <prakhil.tp@gmail.com>
2018-09-25 19:16:38 +0530

    Refactor code

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux';
-import movieReducer from './movieReducer';
-import searchLoadingReducer from './searchLoadingReducer';
+import { combineReducers } from 'redux'
+import movieReducer from './movieReducer'
+import searchLoadingReducer from './searchLoadingReducer'
 
 
 export default combineReducers({
   Movies: movieReducer,
-  SearchLoading: searchLoadingReducer
+  SearchLoading: searchLoadingReducer,
 });
